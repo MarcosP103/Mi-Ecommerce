7ec87fc232fb8044dc037ae04e476e7ba9ca4b29
Marcos Perez <mperezro103@gmail.com>
2023-10-05 12:18:22 -0300

    ultimo ruteo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import NavBar from "./component/NavBar/NavBar";
 import CardList from "./component/CardList/CardList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CardDetailContainer from "./component/CardDetailContainer/CardDetailContainer"
+import CardDetailContainer from "./component/CardDetailContainer/CardDetailContainer";
 
 function App() {
   return (
@@ -10,7 +10,8 @@ function App() {
       <NavBar />
       <Routes>
         <Route exact path="/" element={<CardList />} />
-        <Route exact path="/cat/:cajasacusticas" element={<CardList/>} />
+        <Route exact path="/cat/:catId" element={<CardList />} />
+        <Route exact path="/cards/:cardsId" element={<CardDetailContainer />} />
         <Route path="*" element={<h1>404 NOT FOUND</h1>} />
       </Routes>
     </BrowserRouter>
