c291b4cd1bea4d82368984698d47d698c0b5fbf7
Marcos Perez <mperezro103@gmail.com>
2023-10-27 12:22:32 -0300

    eliminar json

--- src/component/ProductoCarrito/ProductoCarrito.js
+++ src/component/ProductoCarrito/ProductoCarrito.js
@@ -10,7 +10,7 @@ const ProductoCarrito = () => {
       {carrito.map((producto) => (
         <div className="producto" key={producto.id}>
           <h3 className="titulo">{producto.titulo}</h3>
-          <h3 className="precio">{producto.precio}</h3>
+          <h3 className="precio">${producto.precio}</h3>
         </div>
       ))}
     </div>
