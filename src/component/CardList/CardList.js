4b8684c01e2e025e4788f32eb414a04853665910
Marcos Perez <mperezro103@gmail.com>
2023-10-28 12:10:58 -0300

    fire

--- src/component/CardList/CardList.js
+++ src/component/CardList/CardList.js
@@ -1,64 +1,70 @@
 import "./stylecardlist.css";
 import { useState, useEffect } from "react";
 import Cards from "../Cards/Cards";
-import { getProductos, getProductoByCat } from "../../data/data";
 import { useParams } from "react-router-dom";
-import { db } from "../.."
-import { collection, getDocs } from "firebase/firestore"
+import { db } from "../..";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 export default function CardList() {
   const [productos, setProductos] = useState([]);
-
   const { catId } = useParams();
 
   useEffect(() => {
-    const asyncFunc = catId ? getProductoByCat : getProductos;
+    const llamadaFSFil = async () => {
+      const allProducts = [];
+      const todasLasCategorias = [
+        "cajasacusticas",
+        "instrumentos",
+        "microfonos",
+        "consolas",
+        "accesorios"
+      ];
 
-    asyncFunc(catId)
-      .then((response) => {
-        setProductos(response);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+      for (const categoria of todasLasCategorias) {
+        const categoryCollection = collection(db, categoria);
+
+        // Verifica si la categoría es nula o igual a catId antes de cargar los productos
+        if (!catId || categoria === catId) {
+          try {
+            const res = await getDocs(categoryCollection);
+            const items = res.docs.map((elem) => ({ ...elem.data() }));
+            allProducts.push(...items);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      }
+
+      setProductos(allProducts);
+    };
+
+    llamadaFSFil();
   }, [catId]);
-
-
-  const llamadaFS = () => {
-    const itemsCollection = collection(db, "productos")
-    getDocs(itemsCollection).then((res) => {
-        let items = res.docs.map((elm)=>({...elm.data()}))
-        console.log(items)
-    })
-}
-  
-  useEffect(()=>{
-    llamadaFS()
-  }, [])
-
-
 
   return (
     <div className="divmapeo">
-    {productos && productos.length > 0 ? (
-      productos.map((element, i) => (
-        <Cards
-          titulo={element.titulo}
-          imag={element.imag}
-          marca={element.marca}
-          precio={element.precio}
-          desc={element.desc}
-          carac={element.carac}
-          cat={element.cat}
-          key={i}
-          id={element.id}
-        />
-      ))
-    ) : (
-      <p>No hay productos disponibles.</p>
-    )}
-  </div>
+      {productos && productos.length > 0 ? (
+        productos.map((element, i) => (
+          <Cards
+            titulo={element.titulo}
+            imag={element.imag}
+            marca={element.marca}
+            precio={element.precio}
+            desc={element.desc}
+            carac={element.carac}
+            cat={element.cat}
+            key={i}
+            id={element.id}
+          />
+        ))
+      ) : (
+        <p>No hay productos disponibles en esta categoría.</p>
+      )}
+    </div>
   );
 }
+
+
+
 
 
